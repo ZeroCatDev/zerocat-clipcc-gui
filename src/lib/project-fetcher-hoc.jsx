1801d372bb8ea3d552162c993c65047c46fdb0ce
孙悟元 <88357633+SunWuyuan@users.noreply.github.com>
2025-01-19 10:00:54 +0800

    Update project-fetcher-hoc.jsx

--- src/lib/project-fetcher-hoc.jsx
+++ src/lib/project-fetcher-hoc.jsx
@@ -134,7 +134,7 @@ const ProjectFetcherHOC = function (WrappedComponent) {
         setProjectId: PropTypes.func
     };
     ProjectFetcherComponent.defaultProps = {
-        assetHost: 'https://api.codingclip.com/v1/',
+        assetHost: 'https://s4-1.wuyuan.1r.ink/',
         projectHost: 'https://projects.scratch.mit.edu'
     };
 
